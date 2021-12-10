62d184c847dbfe869a4992260e7b0052a0c35d53
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-12-09 23:18:41 -0500

    Final changes

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "components/Appointment/styles.scss";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const cancel = () => {
+    props.onCancel();
+    reset();
+  };
+
+  const reset = () => {
+    console.log("reset");
+    setStudent("");
+    setInterviewer("");
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button onClick={() => props.onSave(student, interviewer)} confirm>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+
+export default function Appointment(props) {
+  console.log(props);
+  return (
+    <Fragment>
+      <Header time={props.time} />
+      <article className="appointment">
+        {" "}
+        {props.interview ? (
+          <Show student={props.interview.student} interview={props.interview} />
+        ) : (
+          <Empty />
+        )}
+      </article>
+    </Fragment>
+  );
+}
