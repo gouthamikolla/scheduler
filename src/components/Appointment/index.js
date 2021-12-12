1c52e8796933091390812abdb94c9338d2778d05
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-12-11 19:19:50 -0500

    Using our Custom Hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,34 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <Fragment>
       <Header time={props.time} />
       <article className="appointment">
         {" "}
-        {props.interview ? (
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
           <Show student={props.interview.student} interview={props.interview} />
-        ) : (
-          <Empty />
+        )}
+        {mode === CREATE && (
+          <Form
+            student=""
+            interviewer={null}
+            interviewers={[]}
+            onSave={() => console.log("on SAVE")}
+            onCancel={() => back(EMPTY)}
+          />
         )}
       </article>
     </Fragment>
